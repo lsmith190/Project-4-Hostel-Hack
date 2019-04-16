63c75ea07936b507db05ab5cbe5b4f2df620fe94
lsmith190 <lindseyr.smith1217@gmail.com>
2019-04-15 21:45:41 -0400

    created views

--- hostel_hack_app/views.py
+++ hostel_hack_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer, HostelSerializer, EventSerializer
+from .models import User, Hostel, Event
 
-# Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class HostelView(viewsets.ModelViewSet):
+    queryset = Hostel.objects.all()
+    serializer_class = HostelSerializer
+
+class EventView(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
