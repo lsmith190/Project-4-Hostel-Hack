1ed22b20f28091ff6d759657f8f4717527b963d0
lsmith190 <lindseyr.smith1217@gmail.com>
2019-04-15 21:48:48 -0400

    register RESTful routes

--- hostel_hack_app/urls.py
+++ hostel_hack_app/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('users', views.UserView)
+router.register('hostels', views.HostelView)
+router.register('events', views.EventView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
